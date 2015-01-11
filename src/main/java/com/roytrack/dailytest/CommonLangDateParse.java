4396f78addb572fe4319de9bc11b6c735185dc65
roytrack <rcm8848@gmail.com>
2015-01-11 14:10:44 +0800

    增加了junit 比较新的包 一个用apache common包的类来快速的转换日期的方法demo

---
+++ src/main/java/com/roytrack/dailytest/CommonLangDateParse.java
@@ -0,0 +1,23 @@
+package com.roytrack.dailytest;
+
+
+import org.apache.commons.lang3.time.DateFormatUtils;
+import org.apache.commons.lang3.time.DateUtils;
+import org.junit.Test;
+
+import java.text.ParseException;
+import java.util.Date;
+
+/**
+ * Created by ruanchangming on 2015/1/11.
+ */
+public class CommonLangDateParse {
+    @Test
+    public void parseDate() throws ParseException {
+        String date="2014/12/12";
+        Date d= DateUtils.parseDateStrictly(date, "yyyy/MM/dd");
+        System.out.println(DateFormatUtils.format(d,"yyyy-MM-dd"));
+    }
+
+}
+

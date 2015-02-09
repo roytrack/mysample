82d8d0911ffaa3b238bec783e74e6a6157b25b23
ruanchangming <rcm8848@gmail.com>
2015-02-09 09:19:48 +0800

    spring bean 注解

---
+++ src/main/java/com/roytrack/spring/core/bean/InstantiationBean.java
@@ -0,0 +1,22 @@
+package com.roytrack.spring.core.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.beans.ConstructorProperties;
+
+/**
+ * Created by roytrack on 2015/2/7.
+ */
+@Getter
+@Setter
+public class InstantiationBean {
+
+   private String name;
+    private String sex;
+    @ConstructorProperties({"name","sex"})
+    public InstantiationBean(String name,String sex){
+        this.name=name;
+        this.sex=sex;
+    }
+}

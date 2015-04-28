24d6f9af6bffc2e39aaaf5f7a276f823502042d6
ruanchangming <rcm8848@gmail.com>
2015-04-28 13:52:47 +0800

    clean up the dependency and a demo for load resource

---
+++ spring/src/main/java/com/roytrack/spring/resource/LoadResource.java
@@ -0,0 +1,35 @@
+package com.roytrack.spring.resource;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+
+
+import java.io.IOException;
+
+/**
+ * Created by ruanchangming on 2015/4/28.
+ */
+public class LoadResource {
+
+    @Test
+    public  void load(){
+        ResourcePatternResolver resolver=new PathMatchingResourcePatternResolver();
+        String scriptContent;
+        String filepath="classpath:org/springframework/web/servlet/config/spring-mvc-3.1.xsd";
+        try {
+            Resource resource=resolver.getResource(filepath);
+            System.out.println(resource.contentLength()+"####"+resource.getFilename()+"###"+resource.getURL());
+            //核心代码 就是resource不能从jar包获得文件 但是可以获得里面的内容
+            scriptContent = IOUtils.toString(resource.getInputStream());
+            System.out.println(scriptContent);
+        } catch (IOException e) {
+            throw new IllegalArgumentException(filepath + " is not exist.", e);
+        }
+
+    }
+
+
+}

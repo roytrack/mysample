e7bd08a363526f9b1cf6d36975178d715d5e3299
ruanchangming <rcm8848@gmail.com>
2015-05-04 11:05:54 +0800

    get month between two date

--- corejava/src/main/java/com/roytrack/dailytest/Date/GetDateFromOther.java
+++ corejava/src/main/java/com/roytrack/dailytest/Date/GetDateFromOther.java
@@ -10,6 +10,7 @@ import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.Temporal;
 import java.time.temporal.TemporalAdjuster;
 import java.util.Calendar;
@@ -42,8 +43,45 @@ public class GetDateFromOther {
         LocalDate begin=LocalDate.of(2014,9,25);
         LocalDate now=LocalDate.now();
         Period betweenDate=Period.between(begin,now);
+
         logger.info("距离发现此bug已过去{}月{}天，共{}天",betweenDate.getMonths(),betweenDate.getDays(),
                 LocalDate.now().toEpochDay()-begin.toEpochDay());
+    }
+
+    @Test
+    public void getMonthBetweenTwoDate(){
+        /**
+         * 如果直接LocalDate.now()那么格式化的话只有年月日
+         * 需要写成 LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))+" 00:00:00"
+         * */
+        String beginStr="2013-02-01 00:00:00";
+        String endStr="2015-05-03 00:00:00";
+        DateTimeFormatter dtf=DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDate begin=LocalDate.parse(beginStr, dtf);
+        LocalDate now=LocalDate.parse(endStr,dtf);
+        Period betweenDate=Period.between(begin,now);
+        System.out.println(betweenDate.toTotalMonths() +"    "+begin.toString()+"   "+now.toString());
+//        for(int i=0;i<betweenDate.toTotalMonths();i++){
+//            System.out.println(begin.toString());
+//            begin=begin.plusMonths(1);
+//        }
+        //获取两个日期之间的所有月 两边都是闭区间
+        for(int i=0;begin.isBefore(now);i++){
+            if(i>0){
+                begin=begin.plusMonths(1);
+                if(begin.isAfter(now)){
+                    break;
+                }
+            }
+            System.out.println(begin.format(DateTimeFormatter.ofPattern("yyyyMM")));
+            System.out.println(begin.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        }
+
+
+
+
+
+
     }
 
 }

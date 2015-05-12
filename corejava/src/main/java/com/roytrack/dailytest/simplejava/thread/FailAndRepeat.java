23e279c66e4f97ff7eb0f93c3511c34c14eac671
ruanchangming <rcm8848@gmail.com>
2015-05-12 16:45:30 +0800

    thread test and modify pom

---
+++ corejava/src/main/java/com/roytrack/dailytest/simplejava/thread/FailAndRepeat.java
@@ -0,0 +1,43 @@
+package com.roytrack.dailytest.simplejava.thread;
+
+/**
+ * Created by ruanchangming on 2015/5/11.
+ */
+public class FailAndRepeat {
+    public static void main(String[] args) {
+       new ReDo(10,"ten time");
+        new ReDo(11,"eleven time");
+    }
+
+
+}
+class ReDo implements Runnable{
+    boolean success=false;
+    int a;
+    String b;
+    public ReDo(int a,String b){
+        this.a=a;
+        this.b=b;
+        new Thread(this,b).start();
+    }
+
+    @Override
+    public void run() {
+        while (!success){
+            try{
+                for(int i=0;i<11;i++){
+                    System.out.println("thread "+b+"  "+(a/(a-i)));
+                }
+                success=true;
+            }catch (Exception e){
+                try {
+                    Thread.sleep(10000L);
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+        System.out.println("====="+b);
+
+    }
+}

778be34ac353a2c6901348eb25cdb05ad7996b49
roytrack <rcm8848@gmail.com>
2016-03-21 18:06:18 +0800

    1.fake nio class

--- netty/src/main/java/com/roytrack/netty/fnio2_2_2/TimeServer.java
+++ netty/src/main/java/com/roytrack/netty/fnio2_2_2/TimeServer.java
@@ -1,7 +1,27 @@
 package com.roytrack.netty.fnio2_2_2;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 /**
  * Created by ruanchangming on 2016/03/21.
  */
 public class TimeServer {
+    public static void main(String[] args) {
+        int port=9009;
+        if(args!=null&&args.length>0){
+            try{
+                port=Integer.valueOf(args[0]);
+            }catch (NumberFormatException e){
+
+            }
+        }
+        ServerSocket server=null;
+        try{
+            server=new ServerSocket(port);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }

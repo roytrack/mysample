0e9a24dcc3397e73384488aed9c2e30e2f154aa5
roytrack <rcm8848@gmail.com>
2016-03-28 23:26:09 +0800

    netty server

--- netty/src/main/java/com/roytrack/netty/netty3_2/TimeServer.java
+++ netty/src/main/java/com/roytrack/netty/netty3_2/TimeServer.java
@@ -2,25 +2,26 @@ package com.roytrack.netty.netty3_2;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 /**
  * Created by ruanchangming on 2016-3-25.
  */
 public class TimeServer {
-    private int port;
     public  void bind(int port){
         EventLoopGroup bossGroup=new NioEventLoopGroup();
         EventLoopGroup workerGroup =new NioEventLoopGroup();
-
+        try {
         ServerBootstrap b=new ServerBootstrap();
         b.group(bossGroup,workerGroup).channel(NioServerSocketChannel.class)
                 .option(ChannelOption.SO_BACKLOG,1024)
-                .childHandler(new ChildChannelHandler());
-        try {
+                .childHandler(new ChildChannelhandler());
+
             ChannelFuture f=b.bind(port).sync();
             f.channel().closeFuture().sync();
         } catch (InterruptedException e) {
@@ -31,6 +32,17 @@ public class TimeServer {
         }
 
     }
+
+    private  class ChildChannelhandler extends ChannelInitializer<SocketChannel>{
+
+        @Override
+        protected void initChannel(SocketChannel socketChannel) throws Exception {
+            socketChannel.pipeline().addLast(new TimeServerHandler());
+        }
+    }
+
+
+
     public static void main(String[] args) {
         int port=9009;
         if(null!=args&args.length>1){

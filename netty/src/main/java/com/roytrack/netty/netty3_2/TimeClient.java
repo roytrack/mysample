4f345e58738852422325226f1ed6ae305ae62c35
roytrack <rcm8848@gmail.com>
2016-05-04 18:16:58 +0800

    netty nio server

---
+++ netty/src/main/java/com/roytrack/netty/netty3_2/TimeClient.java
@@ -0,0 +1,51 @@
+package com.roytrack.netty.netty3_2;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+
+/**
+ * Created by ruanchangming on 2016-4-21.
+ */
+public class TimeClient {
+
+    public void connect(int port, String host) throws InterruptedException {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+
+
+            Bootstrap b = new Bootstrap();
+            b.group(group).channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<io.netty.channel.socket.SocketChannel>() {
+                        @Override
+                        protected void initChannel(io.netty.channel.socket.SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new TimeClientHandler());
+                        }
+                    });
+            ChannelFuture f = b.connect(host, port).sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        int port=9009;
+        if(args!=null&&args.length>0){
+            try{
+                port=Integer.parseInt(args[0]);
+            }catch (NumberFormatException e){
+
+            }
+        }
+        new TimeClient().connect(port,"127.0.0.1");
+    }
+
+}
---
+++ netty/src/main/java/com/roytrack/netty/netty3_2/TimeClientHandler.java
@@ -0,0 +1,10 @@
+package com.roytrack.netty.netty3_2;
+
+import io.netty.channel.ChannelHandlerAdapter;
+
+/**
+ * Created by ruanchangming on 2016-05-04.
+ */
+public class TimeClientHandler extends ChannelHandlerAdapter{
+
+}

2abb0f39599393bd9ae403c171dc68bed8d6733f
roytrack <rcm8848@gmail.com>
2016-05-06 13:33:18 +0800

    charAt 方法报错

--- corejava/src/main/java/com/roytrack/dailytest/simplejava/string/IndexOf.java
+++ corejava/src/main/java/com/roytrack/dailytest/simplejava/string/IndexOf.java
@@ -7,5 +7,7 @@ public class IndexOf {
     public static void main(String[] args) {
         String target="ASCJ_32323_3432_32343";
         System.out.println(target.indexOf("ASCJ"));
+        System.out.println(target.length());
+        target.charAt(target.length());
     }
 }

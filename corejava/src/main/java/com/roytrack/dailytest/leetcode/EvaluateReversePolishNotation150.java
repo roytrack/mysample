c562149cf24e8fb911107db6cdf62ed58dcf48dd
roytrack <rcm8848@gmail.com>
2016-05-23 18:06:41 +0800

    RPN only beat 25%

---
+++ corejava/src/main/java/com/roytrack/dailytest/leetcode/EvaluateReversePolishNotation150.java
@@ -0,0 +1,38 @@
+package com.roytrack.dailytest.leetcode;
+
+import java.util.Stack;
+
+/**
+ * Created by ruanchangming on 2016-05-23.
+ */
+public class EvaluateReversePolishNotation150 {
+    public static void main(String[] args) {
+        String[] expression={"2", "1", "+", "3", "*"};
+        EvaluateReversePolishNotation150 e=new EvaluateReversePolishNotation150();
+        System.out.println(e.evalRPN(expression));
+        String[] expression2={"4", "13", "5", "/", "+"};
+        System.out.println(e.evalRPN(expression2));
+    }
+
+    public int evalRPN(String[] tokens) {
+        Stack<String> stack=new Stack<>();
+        for(int i=0;i<tokens.length;i++){
+            if("*/+-".contains(tokens[i])){
+                int a=Integer.valueOf(stack.pop());
+                int b=Integer.valueOf(stack.pop());
+                switch (tokens[i]){
+                    case "*":a=b*a;break;
+                    case "/":a=b/a;break;
+                    case "+":a=b+a;break;
+                    case "-":a=b-a;break;
+                }
+                stack.push(String.valueOf(a));
+            }else{
+                stack.push(tokens[i]);
+            }
+
+        }
+        int result=Integer.valueOf(stack.pop());
+        return  result;
+    }
+}

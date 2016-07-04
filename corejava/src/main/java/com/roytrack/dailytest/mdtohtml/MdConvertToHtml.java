732f6fab059f7c5887fe22ae7a7ed742a8d4ad29
roytrack <rcm8848@gmail.com>
2016-07-04 22:26:57 +0800

    添加mdtohtml依赖

--- corejava/src/main/java/com/roytrack/dailytest/mdtohtml/MdConvertToHtml.java
+++ corejava/src/main/java/com/roytrack/dailytest/mdtohtml/MdConvertToHtml.java
@@ -1,7 +1,22 @@
 package com.roytrack.dailytest.mdtohtml;
 
+import org.tautua.markdownpapers.Markdown;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.Reader;
+import java.io.Writer;
+
 /**
  * Created by roytrack on 2016-07-04.
  */
 public class MdConvertToHtml {
+
+    public static void main(String[] args) throws Exception{
+        Reader in = new FileReader("D:\\wp\\sample\\corejava\\src\\main\\java\\com\\roytrack\\dailytest\\mdtohtml\\142.md");
+        Writer out = new FileWriter("D:\\wp\\sample\\corejava\\src\\main\\java\\com\\roytrack\\dailytest\\mdtohtml\\out.html");
+
+        Markdown md = new Markdown();
+        md.transform(in, out);
+    }
 }

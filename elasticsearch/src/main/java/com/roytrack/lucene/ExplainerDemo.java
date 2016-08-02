e75ce67618c84858aef9ad409cd49c71cf4ff11d
roytrack <rcm8848@gmail.com>
2016-08-02 15:22:13 +0800

    crack mybatis plugin

---
+++ elasticsearch/src/main/java/com/roytrack/lucene/ExplainerDemo.java
@@ -0,0 +1,9 @@
+package com.roytrack.lucene;
+
+/**
+ * Created by roytrack on 2016-08-02.
+ */
+public class ExplainerDemo {
+
+
+}
---
+++ mybatis/src/main/java/com/roytrack/mybatis/crack/CrackPlugin.java
@@ -0,0 +1,23 @@
+package com.roytrack.mybatis.crack;
+
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+
+/**
+ * Created by roytrack on 2016-08-02.
+ */
+public class CrackPlugin {
+    public static void main(String[] args) throws Exception {
+        ClassPool pool = ClassPool.getDefault();
+        CtClass c = pool.get("com.seventh7.mybatis.util.JavaUtils");
+        CtMethod m = c.getDeclaredMethod("refValid");
+        m.setBody("{ validated = true; valid = true; return valid; }");
+        c.writeFile("D:\\tmp\\999");
+
+        CtClass cc = pool.get("com.seventh7.mybatis.service.JavaService");
+        CtMethod mm = cc.getDeclaredMethod("stop");
+        mm.setBody("{ return; }");
+        cc.writeFile("D:\\tmp\\999");
+    }
+}

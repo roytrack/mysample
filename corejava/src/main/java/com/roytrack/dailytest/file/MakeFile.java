40e321a9158f941a7621aef0e1cb046c6c1baba3
roytrack <rcm8848@gmail.com>
2016-09-05 18:32:30 +0800

    see unicode

--- corejava/src/main/java/com/roytrack/dailytest/file/MakeFile.java
+++ corejava/src/main/java/com/roytrack/dailytest/file/MakeFile.java
@@ -4,11 +4,12 @@ import java.io.File;
 import java.io.IOException;
 
 /**
- * Created by ruanchangming on 2015/11/18.
+ * Created by roytrack on 2015/11/18.
  */
 public class MakeFile {
     public static void main(String[] args) throws IOException {
         String path="/letv/deployment/cj_file/export.txt";
+
         File f=new File(path);
         f.mkdirs();
         f=new File("/letv/deployment/cj_file/exporsst.txt");
--- corejava/src/main/java/com/roytrack/dailytest/simplejava/string/SeeUnicode.java
+++ corejava/src/main/java/com/roytrack/dailytest/simplejava/string/SeeUnicode.java
@@ -1,5 +1,7 @@
 package com.roytrack.dailytest.simplejava.string;
 
+import org.apache.commons.lang3.CharUtils;
+
 /**
  * Created by ruanchangming on 2016-3-29.
  */
@@ -7,5 +9,11 @@ public class SeeUnicode {
     public static void main(String[] args) {
 
         System.out.println("\\u7a0e\\u540e\\u91d1\\u989d");
+
+        String target="万岁万岁";
+        for(int i=0;i<target.length();i++){
+            System.out.println(CharUtils.unicodeEscaped(target.charAt(i)));
+        }
+
     }
 }

d71bd1c3ad2f2f6af0349d4da9f9af64589cee88
roytrack <rcm8848@gmail.com>
2016-09-17 18:18:27 +0800

    complex resultmap in mybatis

---
+++ mybatis/src/main/java/com/roytrack/mybatis/complexMap/mapper/BlogMapper.java
@@ -0,0 +1,7 @@
+package com.roytrack.mybatis.complexMap.mapper;
+
+/**
+ * Created by roytrack on 2016-09-17.
+ */
+public interface BlogMapper {
+}
---
+++ mybatis/src/main/java/com/roytrack/mybatis/complexMap/model/Blog.java
@@ -0,0 +1,7 @@
+package com.roytrack.mybatis.complexMap.model;
+
+/**
+ * Created by roytrack on 2016-09-17.
+ */
+public class Blog {
+}

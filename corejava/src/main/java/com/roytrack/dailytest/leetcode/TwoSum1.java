d5bb4358c0cc28e767a0167f6742b4f9f54aec38
roytrack <rcm8848@gmail.com>
2017-01-03 11:23:37 +0800

    leetcode 2017 first resolve

---
+++ corejava/src/main/java/com/roytrack/dailytest/leetcode/TwoSum1.java
@@ -0,0 +1,21 @@
+package com.roytrack.dailytest.leetcode;
+
+/**
+ * 新的一年 刷leetcode第一题 嘿嘿
+ * Created by roytrack on 2017-01-03.
+ */
+public class TwoSum1 {
+    public int[] twoSum(int[] nums, int target) {
+        int [] result=new  int[2];
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    result[0]=i;
+                    result[1]=j;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}

3e708ee09f5b51689cb960b4541a8f577ff0fce3
roytrack <rcm8848@gmail.com>
2017-01-06 17:41:38 +0800

    leetcode 2 half part

---
+++ corejava/src/main/java/com/roytrack/dailytest/leetcode/AddTwoNumbers_2.java
@@ -0,0 +1,20 @@
+package com.roytrack.dailytest.leetcode;
+
+/**
+ * Created by roytrack on 2017-01-04.
+ */
+public class AddTwoNumbers_2 {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        return null;
+    }
+
+}
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(int val){
+        this.val=val;
+    }
+}

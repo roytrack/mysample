a4a0968b39048f5e052efeb07948ef237253bcfe
roytrack <rcm8848@gmail.com>
2017-01-09 18:39:36 +0800

    leetcode 2 half part

--- corejava/src/main/java/com/roytrack/dailytest/leetcode/AddTwoNumbers_2.java
+++ corejava/src/main/java/com/roytrack/dailytest/leetcode/AddTwoNumbers_2.java
@@ -1,13 +1,57 @@
 package com.roytrack.dailytest.leetcode;
 
 /**
+ * You are given two non-empty linked lists representing two non-negative integers.
+ * The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+ *You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ *Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ *Output: 7 -> 0 -> 8
+ *
  * Created by roytrack on 2017-01-04.
  */
 public class AddTwoNumbers_2 {
 
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    public static void main(String[] args) {
+        ListNode l11=new ListNode(2);
+        ListNode l12=new ListNode(4);
+        l11.next=l12;
+        ListNode l13=new ListNode(3);
+        l12.next=l13;
+        ListNode l21=new ListNode(5);
+        ListNode l22=new ListNode(6);
+        l21.next=l22;
+        ListNode l23=new ListNode(4);
+        l22.next=l23;
+        AddTwoNumbers_2 s=new AddTwoNumbers_2();
+        ListNode result=s.addTwoNumbers(l11,l21);
+        while (result.next!=null){
+            System.out.println(result.val+"--->");
+            result=result.next;
+        }
 
-        return null;
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode resultNode=null,tmpNode=null;
+        int sum,addFlag=0;
+        while(l1.next!=null||l2.next!=null){
+            sum=(l1.next==null?0:l1.val)+(l2.next==null?0:l2. val)+addFlag;
+            if(sum/10==1){
+                addFlag=1;
+            }
+            if(resultNode==null){
+                resultNode=new ListNode(sum%10);
+                tmpNode=resultNode;
+            }else{
+                ListNode newNode=new ListNode(sum%10);
+                tmpNode.next=newNode;
+                tmpNode=newNode;
+            }
+            l1=l1.next;
+            l2=l2.next;
+        }
+
+        return resultNode;
     }
 
 }

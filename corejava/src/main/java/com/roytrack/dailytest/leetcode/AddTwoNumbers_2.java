54c34e66465a9e9229a3cf70c9d69d1a38145d60
roytrack <rcm8848@gmail.com>
2017-01-11 16:39:27 +0800

    leetcode 2 worked

--- corejava/src/main/java/com/roytrack/dailytest/leetcode/AddTwoNumbers_2.java
+++ corejava/src/main/java/com/roytrack/dailytest/leetcode/AddTwoNumbers_2.java
@@ -12,16 +12,18 @@ package com.roytrack.dailytest.leetcode;
 public class AddTwoNumbers_2 {
 
     public static void main(String[] args) {
-        ListNode l11=new ListNode(2);
-        ListNode l12=new ListNode(4);
-        l11.next=l12;
-        ListNode l13=new ListNode(3);
-        l12.next=l13;
+        ListNode l11=new ListNode(5);
         ListNode l21=new ListNode(5);
-        ListNode l22=new ListNode(6);
-        l21.next=l22;
-        ListNode l23=new ListNode(4);
-        l22.next=l23;
+//        ListNode l11=new ListNode(2);
+//        ListNode l12=new ListNode(4);
+//        l11.next=l12;
+//        ListNode l13=new ListNode(3);
+//        l12.next=l13;
+//        ListNode l21=new ListNode(5);
+//        ListNode l22=new ListNode(6);
+//        l21.next=l22;
+//        ListNode l23=new ListNode(4);
+//        l22.next=l23;
         AddTwoNumbers_2 s=new AddTwoNumbers_2();
         ListNode result=s.addTwoNumbers(l11,l21);
         while (null!=result){
@@ -61,7 +63,9 @@ public class AddTwoNumbers_2 {
                 tmpNode.next=newNode;
                 tmpNode=newNode;
             }
+            if(l1!=null)
             l1=l1.next;
+            if(l2!=null)
             l2=l2.next;
         }
 

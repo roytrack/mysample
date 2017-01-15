b7887676111345a23396409fb0cbfd40fb717639
roytrack <rcm8848@gmail.com>
2017-01-15 15:48:59 +0800

    leetcode 3 begin

---
+++ corejava/src/main/java/com/roytrack/dailytest/leetcode/LongestSubStringWithoutRepeating_3.java
@@ -0,0 +1,18 @@
+package com.roytrack.dailytest.leetcode;
+
+/**
+ * Created by roytrack on 2017-01-15.
+ */
+public class LongestSubStringWithoutRepeating_3 {
+
+    public static void main(String[] args) {
+        LongestSubStringWithoutRepeating_3 l=new LongestSubStringWithoutRepeating_3();
+        String demoStr="qwereax";
+        l.lengthOfLongestSubstring(demoStr);
+    }
+
+    public int lengthOfLongestSubstring(String s) {
+
+        return 0;
+    }
+}

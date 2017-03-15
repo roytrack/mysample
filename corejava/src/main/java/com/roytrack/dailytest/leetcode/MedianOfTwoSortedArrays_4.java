cc083a250c47ad3abcfc17c57a7d2eb4afdfbe07
roytrack <rcm8848@gmail.com>
2017-03-15 14:47:09 +0800

    leetcode 4 begin

--- corejava/src/main/java/com/roytrack/dailytest/leetcode/MedianOfTwoSortedArrays_4.java
+++ corejava/src/main/java/com/roytrack/dailytest/leetcode/MedianOfTwoSortedArrays_4.java
@@ -1,6 +1,11 @@
 package com.roytrack.dailytest.leetcode;
 
 /**
+ * 找到两个顺序数组的中间值
+ * 例如 nums1=[1,3]
+ *      nums2=[2]  中值为2
+ *      nums1=[1,2]
+ *      nums2=[3,4] 中值为（2+3）/2=2.5
  * Created by roytrack on 2017-01-16.
  */
 public class MedianOfTwoSortedArrays_4 {

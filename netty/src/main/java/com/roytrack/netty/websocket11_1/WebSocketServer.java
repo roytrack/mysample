ffa4537226c69983e1c08b0e76fe2865d5bbe248
roytrack <rcm8848@gmail.com>
2017-03-24 16:16:24 +0800

    let's smell websocket

---
+++ netty/src/main/java/com/roytrack/netty/websocket11_1/WebSocketServer.java
@@ -0,0 +1,49 @@
+package com.roytrack.netty.websocket11_1;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+/**
+ * Created by roytrack on 2017-03-24.
+ */
+public class WebSocketServer {
+    private static final String url="10.87.156.46";
+
+    public void run(int port) throws InterruptedException {
+        EventLoopGroup bossGroup=new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup=new NioEventLoopGroup();
+        ServerBootstrap b=new ServerBootstrap();
+        try{
+            b.group(bossGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast("http-codec", new HttpServerCodec())
+                                    .addLast("aggregator", new HttpObjectAggregator(65536))
+                                    .addLast("http-chunked", new ChunkedWriteHandler())
+                                    .addLast("handler", new WebSocketServerHandler());
+                        }
+                    });
+            Channel ch= b.bind(url,port).sync().channel();
+            System.out.println("Web socket server started at "+url+":"+port+".");
+            ch.closeFuture().sync();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        int port=8080;
+        new WebSocketServer().run(port);
+    }
+}
---
+++ netty/src/main/java/com/roytrack/netty/websocket11_1/WebSocketServerHandler.java
@@ -0,0 +1,98 @@
+package com.roytrack.netty.websocket11_1;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.*;
+import io.netty.handler.codec.http.websocketx.*;
+import io.netty.util.CharsetUtil;
+
+import java.time.LocalDateTime;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by roytrack on 2017-03-24.
+ */
+public class WebSocketServerHandler extends SimpleChannelInboundHandler<Object> {
+
+    private static final Logger logger = Logger.getLogger(WebSocketServerHandler.class.getName());
+
+    private WebSocketServerHandshaker handshaker;
+
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+        if (msg instanceof FullHttpRequest) {
+            handleHttpRequest(ctx, (FullHttpRequest) msg);
+        } else if (msg instanceof WebSocketFrame) {
+            handleWebSocketFrame(ctx, (WebSocketFrame) msg);
+        }
+
+
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    private void handleHttpRequest(ChannelHandlerContext ctx, FullHttpRequest req) {
+        if ((!req.decoderResult().isSuccess()) || (!"websocket".equals(req.headers().get("Upgrade")))) {
+            sendHttpResponse(ctx, req, new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.BAD_REQUEST));
+            return;
+        }
+        WebSocketServerHandshakerFactory factory = new WebSocketServerHandshakerFactory("ws://10.87.156.46:8080/websocket", null, false);
+        handshaker = factory.newHandshaker(req);
+        if (handshaker == null) {
+            WebSocketServerHandshakerFactory.sendUnsupportedVersionResponse(ctx.channel());
+        } else {
+            handshaker.handshake(ctx.channel(), req);
+        }
+    }
+
+    private void handleWebSocketFrame(ChannelHandlerContext ctx, WebSocketFrame frame) {
+        if (frame instanceof CloseWebSocketFrame) {
+            handshaker.close(ctx.channel(), (CloseWebSocketFrame) frame.retain());
+        }
+        if (frame instanceof PingWebSocketFrame) {
+            ctx.channel().write(new PongWebSocketFrame(frame.content().retain()));
+            return;
+        }
+        if (!(frame instanceof TextWebSocketFrame)) {
+            throw new UnsupportedOperationException(String.format("% frame types not supported", frame.getClass().getName()));
+        }
+        String request = ((TextWebSocketFrame) frame).text();
+        if (logger.isLoggable(Level.FINE)) {
+            logger.fine(String.format("%s recevied %s", ctx.channel(), request));
+        }
+        ctx.channel().write(new TextWebSocketFrame(request + ",欢迎使用Netty WebSocket服务，现在时刻：" + LocalDateTime.now().toString()));
+    }
+
+    private static void sendHttpResponse(ChannelHandlerContext ctx, FullHttpRequest req, FullHttpResponse resp) {
+        if (resp.status().code() != 200) {
+            ByteBuf buf = Unpooled.copiedBuffer(resp.status().toString(), CharsetUtil.UTF_8);
+            resp.content().writeBytes(buf);
+            buf.release();
+            HttpUtil.setContentLength(resp, resp.content().readableBytes());
+        }
+
+        ChannelFuture f = ctx.channel().writeAndFlush(resp);
+        if (!HttpUtil.isKeepAlive(req) || resp.status().code() != 200) {
+            f.addListener(ChannelFutureListener.CLOSE);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+
+}
+

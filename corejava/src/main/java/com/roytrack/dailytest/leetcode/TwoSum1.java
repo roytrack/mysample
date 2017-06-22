bf3a50a3a7cd6b8162b5382d713990db262f7c17
ruanchangming <ruanchangming@lianjia.com>
2017-06-22 14:59:04 +0800

    resolve travis error

--- corejava/src/main/java/com/roytrack/dailytest/leetcode/TwoSum1.java
+++ corejava/src/main/java/com/roytrack/dailytest/leetcode/TwoSum1.java
@@ -1,5 +1,8 @@
 package com.roytrack.dailytest.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 新的一年 刷leetcode第一题 嘿嘿
  * Created by roytrack on 2017-01-03.
@@ -15,6 +18,20 @@ public class TwoSum1 {
                     break;
                 }
             }
+        }
+        return result;
+    }
+
+    public int[] twoSumBetter(int[] numbers, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (map.containsKey(target - numbers[i])) {
+                result[1] = i + 1;
+                result[0] = map.get(target - numbers[i]);
+                return result;
+            }
+            map.put(numbers[i], i + 1);
         }
         return result;
     }

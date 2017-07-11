d0bd2e9360fce59e0fd75c1fb467a38fb53afbc7
roy <rcm8848@gmail.com>
2017-07-12 00:00:25 +0800

    why hello

---
+++ corejava/src/main/java/com/roytrack/dailytest/simplejava/string/StringShow.java
@@ -0,0 +1,16 @@
+package com.roytrack.dailytest.simplejava.string;
+
+/**
+ * Created by roy on 2017/7/11.
+ */
+public class StringShow {
+    public static void main(String[] args) {
+        String str="hello";
+        String str1=str+"";
+        String str2=str1.intern();
+        String str3=str;
+        String str4=str;
+        str=null;
+        System.out.println(str4);
+    }
+}

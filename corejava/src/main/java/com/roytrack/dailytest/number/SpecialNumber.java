bb415d5094c239c8aaf1c8affd0a7a60a08eddc1
ruanchangming <ruanchangming@lianjia.com>
2017-09-29 10:17:37 +0800

    int divide ,regrex match

--- corejava/src/main/java/com/roytrack/dailytest/number/SpecialNumber.java
+++ corejava/src/main/java/com/roytrack/dailytest/number/SpecialNumber.java
@@ -1,5 +1,7 @@
 package com.roytrack.dailytest.number;
 
+import java.util.regex.Pattern;
+
 /**
  * Created by roytrack on 2016/3/22.
  */
@@ -9,6 +11,19 @@ public class SpecialNumber
         System.out.println(0x00004000);
         System.out.println(0x00000010);
         System.out.println(0x00000040);
+        int a=190;int b=100;
+        int c=a/b;
+        System.out.println(c);
+        String aa="aaa";
+        String bb="2323sd";
+        String cc="5423534 ";
+        System.out.println(isNumeric(aa));
+        System.out.println(isNumeric(bb));
+        System.out.println(isNumeric(cc));
+    }
 
+    public static boolean isNumeric(String str){
+        Pattern pattern = Pattern.compile("[0-9]*");
+        return pattern.matcher(str).matches();
     }
 }

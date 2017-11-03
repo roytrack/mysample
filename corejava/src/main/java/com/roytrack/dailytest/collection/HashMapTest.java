dc12a13742825d63ae35fa05fa1480a7f84a8590
ruanchangming <ruanchangming@lianjia.com>
2017-11-03 14:59:53 +0800

    check when hashmap to resize

---
+++ corejava/src/main/java/com/roytrack/dailytest/collection/HashMapTest.java
@@ -0,0 +1,23 @@
+package com.roytrack.dailytest.collection;
+
+import java.util.HashMap;
+
+public class HashMapTest {
+    public static void main(String[] args) {
+        HashMap<String,String> a=new HashMap();
+        a.put("a1","c");
+        a.put("a2","c");
+        a.put("a3","c");
+        a.put("a4","c");
+        a.put("a5","c");
+        a.put("a6","c");
+        a.put("a7","c");
+        a.put("a8","c");
+        a.put("a9","c");
+        a.put("a10","c");
+        a.put("a11","c");
+        a.put("a12","c");
+        a.put("a13","c");
+        a.put("a14","c");
+    }
+}

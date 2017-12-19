737de21d3566bb4f9f60d145a5f65b0a5f2c19f3
ruanchangming <ruanchangming@lianjia.com>
2017-12-19 15:23:43 +0800

    add hashmap test.

--- corejava/src/main/java/com/roytrack/dailytest/collection/HashMapTest.java
+++ corejava/src/main/java/com/roytrack/dailytest/collection/HashMapTest.java
@@ -29,5 +29,32 @@ public class HashMapTest {
             System.out.println("打印第"+i+"ge key $"+b.getKey()+"$ val $"+b.getValue()+"$");
             i++;
         }
+
+        HashMap<String,String> b=new HashMap<>();
+        b.put("a1","c");
+        b.put("a2","c");
+        b.put("a3","c");
+        b.put("a4","c");
+        b.put("a5","c");
+        b.put("a6","c");
+        b.put("a7","c");
+        b.put("a8","c");
+        b.put("a9","c");
+        b.put("a10","c");
+        b.put("a11","c");
+        b.put("a12","c");
+        b.put("a13","c");
+        b.put("a14","c");
+        b.put("a15","c");
+        b.put("a16","c");
+         itr=b.entrySet().iterator();
+        i=0;
+        while (itr.hasNext()){
+            Map.Entry<String,String> entry=itr.next();
+            System.out.println("打印第"+i+"ge key $"+entry.getKey()+"$ val $"+entry.getValue()+"$");
+            i++;
+        }
     }
+
+
 }

0682af1797e933736d7b957771978885509c3c9b
ruanchangming <ruanchangming@lianjia.com>
2018-01-20 16:21:41 +0800

    more bigdecimal demo

--- corejava/src/main/java/com/roytrack/dailytest/BigDecimalToStringNoE.java
+++ corejava/src/main/java/com/roytrack/dailytest/BigDecimalToStringNoE.java
@@ -7,9 +7,16 @@ import java.math.BigDecimal;
  */
 public class BigDecimalToStringNoE {
     public static void main(String[] args) {
-        System.out.println(String_String_Enum.getEnum("2000").getDesc());
-        System.out.println((new BigDecimal("100000000000000000").stripTrailingZeros().toPlainString()));
-        System.out.println((new BigDecimal("100000000000000000").stripTrailingZeros().toString()));
-        System.out.println((new BigDecimal("100000000000000000").stripTrailingZeros().toEngineeringString()));
+//        System.out.println(String_String_Enum.getEnum("2000").getDesc());
+//        System.out.println((new BigDecimal("100000000000000000").stripTrailingZeros().toPlainString()));
+//        System.out.println((new BigDecimal("100000000000000000").stripTrailingZeros().toString()));
+//        System.out.println((new BigDecimal("100000000000000000").stripTrailingZeros().toEngineeringString()));
+
+        double a=12.2233344;
+        a=3014565.8499999978;
+        BigDecimal b = new BigDecimal(a);
+        //保留2位小数
+        double result = b.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+        System.out.println(result);
     }
 }

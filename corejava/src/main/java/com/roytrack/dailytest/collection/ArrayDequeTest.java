9c765b5cf6f5c9911f7c16f09fce4740f7e16e1e
ruanchangming <ruanchangming@huobi.com>
2018-06-21 14:54:43 +0800

    add arrayDeque demo

---
+++ corejava/src/main/java/com/roytrack/dailytest/collection/ArrayDequeTest.java
@@ -0,0 +1,27 @@
+package com.roytrack.dailytest.collection;
+
+import java.util.ArrayDeque;
+
+public class ArrayDequeTest {
+
+  public static void main(String[] args) {
+    ArrayDeque<Long> deque=new ArrayDeque<>(3);
+    deque.push(1L);
+    deque.push(2L);
+    deque.push(3L);
+    deque.push(4L);
+    System.out.println(deque);
+    deque.push(5L);
+    while (deque.size()>2){
+      deque.pollLast();
+      System.out.println(deque);
+      System.out.println("######");
+    }
+
+    Long l1=1L;
+    if(l1.equals(1L)){
+      System.out.println("unbox");
+    }
+  }
+
+}

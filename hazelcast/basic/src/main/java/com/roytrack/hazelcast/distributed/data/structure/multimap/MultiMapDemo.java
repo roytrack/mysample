1d60f4d91cb63cd991e06abae3f1d1ba853dabfe
roytrack <rcm8848@gmail.com>
2019-01-07 00:35:42 +0800

    hazelcast multimap demo

---
+++ hazelcast/basic/src/main/java/com/roytrack/hazelcast/distributed/data/structure/multimap/MultiMapDemo.java
@@ -0,0 +1,23 @@
+package com.roytrack.hazelcast.distributed.data.structure.multimap;
+
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.MultiMap;
+
+import java.util.Collection;
+
+public class MultiMapDemo {
+  public static void main(String[] args) {
+    HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance();
+    MultiMap<String, String> map = hazelcastInstance.getMultiMap("map");
+    map.put("a", "1");
+    map.put("a", "2");
+    map.put("b", "3");
+    System.out.println("PutMember:Done");
+
+    for (String key : map.keySet()) {
+      Collection<String> values = map.get(key);
+      System.out.printf("%s -> %s\n", key, values);
+    }
+  }
+}
